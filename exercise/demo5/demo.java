63eb2f805178de73029069a96d4b30a9271c70ed
Weibozzz <739291780@qq.com>
2019-03-05 22:49:19 +0800

    refact:位运算

---
+++ exercise/demo5/demo.java
@@ -0,0 +1,29 @@
+class Operate {
+    public static void main(String[] args){
+        int x = 1;
+        /*
+          运算比较
+          &：且(两边都运算)
+          &&： (左边为假，右边不运算)，结果都一样
+          |：或
+          ||：(左边为真，右边不运算)，结果都一样
+          ^: 异或
+            true ^ true = false;
+            false ^ false = false;
+            true ^ false = true;
+            false ^ true = true;
+            特点：符号两边结果一样为false
+           !:非
+
+        
+        */ 
+        /*
+            位运算 % |
+            6 & 3 = 2；
+            开发几乎用不到
+             
+        
+        */ 
+        System.out.println(x);
+    }
+}
