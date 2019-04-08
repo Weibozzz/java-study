128e2ebcc1b7e08dbfdacff98c7d2b1f4fe6a841
Weibozzz <739291780@qq.com>
2019-04-08 15:50:26 +0800

    feat:类型转换

---
+++ faceObject/demo18/demo.java
@@ -0,0 +1,31 @@
+/**
+ * 多态：类型转换
+ */
+class Aniaml{
+    void eat(){
+        System.out.println("吃");
+    }
+    void run(){
+        System.out.println("跑");
+    }
+  }
+  
+  class Dog extends Aniaml{
+    void eat(){
+        System.out.println("狗吃东西");
+    }
+    void seeDoor(){
+        System.out.println("狗看门");
+    }
+  }
+class demo {
+    public static void main(String[] args){
+        Aniaml a = new Dog();
+        a.eat();
+        a.run();
+        Dog d = (Dog) a;
+        d.seeDoor();
+    }
+}
+
+ 
