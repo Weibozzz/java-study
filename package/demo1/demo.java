abbaa319b5b94f309f34a83ca7f6295639c3a6b5
Weibozzz <739291780@qq.com>
2019-05-05 14:50:41 +0800

    feact:包的基础使用

---
+++ package/demo1/demo.java
@@ -0,0 +1,11 @@
+/**
+ * 包的使用
+ * javac -d . Demo.java
+ * java mypackage.Demo
+ * */
+package mypackage;
+class Demo{
+    public static void main(String[] args){
+        System.out.println("hello package");
+    }
+}
