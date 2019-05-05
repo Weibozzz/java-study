a0c027920c8fb9f7bf19f274795d0e9294533575
Weibozzz <739291780@qq.com>
2019-05-05 16:52:37 +0800

    feact:单线程实例=》多线程

---
+++ ticket/demo2/demo.java
@@ -0,0 +1,31 @@
+/**
+ * 创建多线程
+ * 1.定义一个类继承Thread类
+ * 2.覆盖Thread类中的run方法
+ * 3.覆盖创建Thread的子类对象创建线程
+ * 4.调用start方法开启线程并调用线程的run方法执行
+ *
+ * 可以通过Thread的getName()方法获得线程名称
+ */
+class Test extends Thread {
+    private String name;
+
+    Test(String name) {
+        this.name = name;
+    }
+
+    public void run() {
+        for (int x = 0; x < 10; x++) {
+            System.out.println(name + "...x=" + x + ";name=" + getName());
+        }
+    }
+}
+
+class Demo {
+    public static void main(String[] args) {
+        Test t1 = new Test("旺财");
+        Test t2 = new Test("xiaoqiang");
+        t1.start();
+        t2.start();
+    }
+}
