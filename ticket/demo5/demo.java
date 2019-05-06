a7cbe6c6d2426b804bb15f8d93e692f6d0d2a0c0
Weibozzz <739291780@qq.com>
2019-05-06 14:50:50 +0800

    fear:线程安全例子示例

---
+++ ticket/demo5/demo.java
@@ -0,0 +1,31 @@
+/***
+ * 线程安全
+ * 需求：两个储户去银行每次都存钱100。共存三次
+ */
+class Bank{
+    private int sum;
+    Object obj = new Object();
+    public void add(int num){
+        synchronized(obj){
+            sum=sum+num;
+            System.out.println("sum="+sum);
+        }
+    }
+}
+class Cus implements Runnable{
+    private Bank b =new Bank();
+    public void run(){
+        for (int i=0;i<3;i++){
+            b.add(100);
+        }
+    }
+}
+class Demo{
+    public static void main(String[] args){
+        Cus c =new Cus();
+        Thread t1 = new Thread(c);
+        Thread t2 = new Thread(c);
+        t1.start();
+        t2.start();
+    }
+}
