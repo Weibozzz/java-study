b6bad67f691e63032fd7705ee60f1f9efa9ff029
Weibozzz <739291780@qq.com>
2019-05-08 16:08:21 +0800

    refact:多线程1.5版本新特性锁lock

--- thread/demo2/demo.java
+++ thread/demo2/demo.java
@@ -1,42 +1,50 @@
-/**
- * 生产者，消费者
- *
- * 多生产者，多消费者的问题
- * if判断标记，只有一次，会导致不该运行的线程运行，出现了错误的情况
- * while判断标记，解决了线程执行权后，是否要运行
- *
- * notify:只能唤醒一个线程，如果只唤醒了本方就没有意义，而且while判断标记+notify会导致死锁。
- * notifyAll:本线程一定会唤醒对方线程
- */
+import java.util.concurrent.locks.*;
+
 class Resource {
     private String name;
     private int count = 1;
     private boolean flag = false;
 
-    public synchronized void set(String name) {
-        while (flag) {
-            try {
-                this.wait();
-            } catch (InterruptedException e) {
+    // 创建一个锁对象
+    Lock lock = new ReentrantLock();
+    // 通过已有的锁获取锁上的监视器对象
+    Condition con = lock.newCondition();
+
+    public void set(String name) {
+        lock.lock();
+        try {
+            while (flag) {
+                try {
+                    con.await();
+                } catch (InterruptedException e) {
+                }
             }
+            this.name = name + count;
+            count++;
+            System.out.println(Thread.currentThread().getName() + "...生产者..." + this.name);
+            flag = true;
+            con.signalAll();
+        } finally {
+            lock.unlock();
         }
-        this.name = name + count;
-        count++;
-        System.out.println(Thread.currentThread().getName() + "...生产者..." + this.name);
-        flag = true;
-        notifyAll();
     }
 
-    public synchronized void out() {
-        while (!flag) {
-            try {
-                this.wait();
-            } catch (InterruptedException e) {
+    public void out() {
+        lock.lock();
+        try {
+
+            while (!flag) {
+                try {
+                    con.await();
+                } catch (InterruptedException e) {
+                }
             }
+            System.out.println(Thread.currentThread().getName() + "...消费者......" + this.name);
+            flag = false;
+            con.signalAll();
+        } finally {
+            lock.unlock();
         }
-        System.out.println(Thread.currentThread().getName() + "...消费者......" + this.name);
-        flag = false;
-        notifyAll();
     }
 }
 
